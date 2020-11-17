54d23c3b71ea56e7c81733b1ed1cc05362101c0c
guilhermemart <gmartinsfilho@gmail.com>
2020-11-17 16:35:04 -0300

    commit 17112020-2 gitignore

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')6!e@1c#=&d52*_j&!$sc$w!91v8r_t5to@!8lsa9!!^t9#lh2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
