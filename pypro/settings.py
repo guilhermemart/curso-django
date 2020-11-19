9df9dfbb22989128de1e446b148434b66b10776a
guilhermemart <gmartinsfilho@gmail.com>
2020-11-18 23:24:36 -0300

    commit 18112020-3

--- pypro/settings.py
+++ pypro/settings.py
@@ -128,6 +128,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
